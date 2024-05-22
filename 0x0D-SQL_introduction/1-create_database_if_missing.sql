83a96e2e16125a7728d83ca4508e0805085364ec
root <bakennefarouq@gmail.com>
2024-05-21 20:59:17 -0700

    .

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Delete the database 'hbtn_0c_0'
--- If 'hbtn_0c_0' doesn't exist, script should not fail
-DROP DATABASE IF EXISTS hbtn_0c_0;
+--  Creates the database 'hbtn_0c_0'
+-- If database already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
